da829dfcebf50a75a1448848e89b47ff7de11038
Leonardo <leopity0@gmail.com>
2023-06-23 18:53:12 -0300

    Page att

--- src/components/header/navbar/index.tsx
+++ src/components/header/navbar/index.tsx
@@ -1,10 +1,109 @@
-import * as React from "react";
-import { useRef } from "react";
-import { motion, sync, useCycle } from "framer-motion";
-import { useDimensions } from "./use-dimensions";
-import { MenuToggle } from "./MenuToggle";
-import { Navigation } from "./Navigation";
+import { useEffect, useRef } from "react";
+import { motion, useCycle} from "framer-motion";
 import styles from "./navbar.module.css"
+
+const useDimensions = (ref: any) => {
+  const dimensions = useRef({ width: 0, height: 0 });
+
+  useEffect(() => {
+    dimensions.current.width = ref.current.offsetWidth;
+    dimensions.current.height = ref.current.offsetHeight;
+  }, []);
+
+  return dimensions.current;
+};
+
+
+const variantes = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+};
+
+const Navigation = () => (
+  <motion.ul className={styles.navul} variants={variantes}>
+    {itemIds.map(i => (
+      <MenuItem i={i} key={i} />
+    ))}
+  </motion.ul>
+);
+
+const itemIds = ['Portifolio', 'Resume', 'About', 'Contact'];
+
+
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
+};
+
+const MenuItem = ({ i }: { i: any}) => {
+  return (
+    <motion.li
+    className={styles.navli}
+      variants={variants}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
+    {i}
+    </motion.li>
+  );
+};
+
+
+const Path = (props: any) => (
+  <motion.path
+    fill="transparent"
+    strokeWidth="3"
+    stroke="hsl(0, 0%, 18%)"
+    strokeLinecap="round"
+    {...props}
+  />
+);
+const MenuToggle = ( { toggle }: { toggle: any } ) => (
+  <button onClick={toggle}>
+    <svg width="23" height="23" viewBox="0 0 23 23">
+      <Path
+        variants={{
+          closed: { d: "M 2 2.5 L 20 2.5" },
+          open: { d: "M 3 16.5 L 17 2.5" }
+        }}
+      />
+      <Path
+        d="M 2 9.423 L 20 9.423"
+        variants={{
+          closed: { opacity: 1 },
+          open: { opacity: 0 }
+        }}
+        transition={{ duration: 0.1 }}
+      />
+      <Path
+        variants={{
+          closed: { d: "M 2 16.346 L 20 16.346" },
+          open: { d: "M 3 2.5 L 17 16.346" }
+        }}
+      />
+    </svg>
+  </button>
+);
+
+
+
 
 const sidebar = {
   open: (height = 1000) => ({
