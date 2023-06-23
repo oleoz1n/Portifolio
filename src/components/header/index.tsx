935e5ff6d234673e2bd1d74cd10e86bfb07a362d
Leonardo <leopity0@gmail.com>
2023-06-23 16:24:44 -0300

    React Portifolio

---
+++ src/components/header/index.tsx
@@ -0,0 +1,56 @@
+import logob from './L1-black.svg'
+import styles from './header.module.css'
+import { useMotionValue, useTransform, motion } from 'framer-motion';
+
+
+
+function Header(){
+    const container = {
+        hidden: { opacity: 1, scale: .8 },
+        visible: {
+          opacity: 1,
+          scale: 1,
+          transition: {
+            delayChildren: 0.3,
+            staggerChildren: 0.2
+          }
+        }
+      }
+        
+      const item = {
+        hidden: { y: 10, opacity: 0 },
+        visible: {
+          y: 0,
+          opacity: 1
+        }
+      }
+
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+    const rotateX = useTransform(y, [-100, 100], [150, -150]);
+    const rotateY = useTransform(x, [-100, 100], [-150, 150]);
+    
+    return (
+        <header className={styles.header}>
+        <motion.ul initial="hidden"
+        animate="visible" 
+        variants={container} 
+        className={styles.headerul}>
+            <motion.li variants={item}><a className={styles.select} href="">Portifolio</a></motion.li>
+            <motion.li variants={item}><a href="">Resume</a></motion.li>
+            <motion.div
+            variants={item} 
+            style={{ x, y, rotateX, rotateY, z: 100 }}
+            drag
+            dragElastic={0.09}
+            dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+            whileTap={{ cursor: 'grabbing' }}>
+                <img src={logob} alt="logo" id='logoHeader' draggable="false" className={styles.logo} />
+            </motion.div>
+            <motion.li variants={item}><a href="">About</a></motion.li>
+            <motion.li variants={item}><a href="">Contact</a></motion.li>
+            </motion.ul>
+            </header>
+    )
+}
+export default Header;
