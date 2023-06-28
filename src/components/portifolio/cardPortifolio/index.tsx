9492583a075bb8c24aeaf214d021308f4a9b7ae0
Leonardo <leopity0@gmail.com>
2023-06-28 19:38:27 -0300

    att card portifolio

--- src/components/portifolio/cardPortifolio/index.tsx
+++ src/components/portifolio/cardPortifolio/index.tsx
@@ -1,4 +1,5 @@
 import styles from './cardPortifolio.module.css'
+import { useState } from 'react';
 import { motion } from "framer-motion";
 
 type CardPortifolioProps = {
@@ -9,32 +10,28 @@ type CardPortifolioProps = {
     id:string;
   };
 
-  function showBotaoPort(id:string){
-    const el = document.getElementById(id);
-    if(el){
-    el.classList.add('showBotaoPort');
-    el.classList.remove('hideBotaoPort')
-}
-}
-
-function hideBotaoPort(id:string){
-    const el = document.getElementById(id);
-    if(el){
-    el.classList.add('hideBotaoPort');
-    el.classList.remove('showBotaoPort');
-}
-}
-
 function CardPortifolio({ imagem, nome, desc, data, id }: CardPortifolioProps) {
+
+  const [isHovered, setHovered] = useState(false)
 
   return (
       <motion.div className={styles.PortifolioCard}
-      onMouseOver={() => showBotaoPort(id)}
-      onMouseOut={() => hideBotaoPort(id)}
+      onMouseOver={() => {setHovered(true)}}
+      onMouseOut={() => {setHovered(false)}}
+      animate={{
+        filter: isHovered ? 'brightness(1)' : 'brightness(0.5)',
+      }}
+      transition={{ duration: isHovered ? 1 : 2}}
       >        
-      <img className={styles.imgPort} src={imagem} alt={nome} draggable='false'/>
+      <img className={styles.imgPort} src={imagem} alt={nome} draggable='false'
+      
+      />
       <motion.div
       id={id} 
+      initial={false}
+      animate={{ y: isHovered ? -8 : -100,
+      opacity: isHovered ? 1 : 0}}
+      transition={{ duration: isHovered ? 1 : 2}}
       className={`${styles.balaoPort} ${'hideBotaoPort'}`}><p className={styles.pBalaoPort}>{data}</p><button className={styles.botaoBalaoPort}>Ver mais</button></motion.div>
       </motion.div>
 
