09c492b0f1332c7ec0e2bc37e83126b1e1dc7d75
oleoz1n <leopity0@gmail.com>
2024-03-10 10:30:00 -0300

    feat: add copyRight simbol in footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,15 @@
-import styles from './footer.module.css'
+import styles from "./footer.module.css";
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <footer className={styles.centralizaFooter}>
-            <div><p>Desenvolvido por oLeoz1n</p></div>
+            <div>
+                <p className={styles.copyRight}>
+                    &copy; Desenvolvido por oLeoz1n
+                </p>
+            </div>
         </footer>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
