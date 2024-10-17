05ae1be3a526522d8d53b1af1c2ef22dec574975
oleoz1n <leopity0@gmail.com>
2024-10-17 18:36:31 -0300

    feat: add cida project

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,80 +1,91 @@
-import { useEffect } from "react";
-import styles from "./modal.module.css";
+import { useEffect } from "react"
+import styles from "./modal.module.css"
 
 interface ModalProps {
-  isOpen: any;
-  setModalOpen: any;
-  children: any;
-  linkSite: any;
-  linkGithub: any;
-  imagem: any;
-  title: any;
-  data: any;
+	isOpen: any
+	setModalOpen: any
+	children: any
+	linkSite: any
+	linkGithub: any
+	imagem: any
+	title: any
+	data: any
 }
 
 export default function Modal({
-  isOpen,
-  setModalOpen,
-  children,
-  linkSite,
-  linkGithub,
-  imagem,
-  title,
-  data
+	isOpen,
+	setModalOpen,
+	children,
+	linkSite,
+	linkGithub,
+	imagem,
+	title,
+	data,
 }: ModalProps) {
-  useEffect(() => {
-    if (isOpen) {
-      document.body.classList.add(styles.disableScroll);
-    } else {
-      document.body.classList.remove(styles.disableScroll);
-    }
-  }, [isOpen]);
+	useEffect(() => {
+		if (isOpen) {
+			document.body.classList.add(styles.disableScroll)
+		} else {
+			document.body.classList.remove(styles.disableScroll)
+		}
+	}, [isOpen])
 
-  if (isOpen) {
-    return (
-      <>
-        <div className={styles.BACKGROUND_STYLE}>
-          <div className={styles.MODAL_STYLE}>
-            <div
-              className={styles.botaoFechar}
-              style={{ cursor: "pointer" }}
-              onClick={setModalOpen}
-            >
-              <span className={`${styles.spamFechar} material-symbols-outlined`}>close</span>
-            </div>
-            <div className={styles.cardModal} >
-              <div className={styles.modalLeft}>
-                <img className={styles.imagemModal} src={imagem} alt={title} />
-                <div className={styles.botoesModal}>
-                  <a className={styles.botaoModal} href={linkSite} target="_blank">
-                    Site
-                  </a>
-                  <a className={styles.botaoModal} href={linkGithub} target="_blank">
-                    Github
-                  </a>
-                </div>
-              </div>
-              <div className={styles.modalRight}>
-                <h1 className={styles.titleModal}>{title}</h1>
-                <div className={styles.textModal} >
-                  {children}
-                </div>
-                <p className={styles.dataModal}>{data}</p>
-              </div>
-              <div className={styles.botoesModal2}>
-                <a className={styles.botaoModal2} href={linkSite} target="_blank">
-                  Site
-                </a>
-                <a className={styles.botaoModal2} href={linkGithub} target="_blank">
-                  Github
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
+	if (isOpen) {
+		console.log("linksite: ", linkSite)
+		return (
+			<>
+				<div className={styles.BACKGROUND_STYLE}>
+					<div className={styles.MODAL_STYLE}>
+						<div
+							className={styles.botaoFechar}
+							style={{ cursor: "pointer" }}
+							onClick={setModalOpen}
+						>
+							<span
+								className={`${styles.spamFechar} material-symbols-outlined`}
+							>
+								close
+							</span>
+						</div>
+						<div className={styles.cardModal}>
+							<div className={styles.modalLeft}>
+								<img
+									className={styles.imagemModal}
+									src={imagem}
+									alt={title}
+								/>
+								<div className={styles.botoesModal}>
+									{linkSite == "" ? null : (
+										<a
+											className={styles.botaoModal2}
+											href={linkSite}
+											target="_blank"
+										>
+											Site
+										</a>
+									)}
+									<a
+										className={styles.botaoModal}
+										href={linkGithub}
+										target="_blank"
+									>
+										Github
+									</a>
+								</div>
+							</div>
+							<div className={styles.modalRight}>
+								<h1 className={styles.titleModal}>{title}</h1>
+								<div className={styles.textModal}>
+									{children}
+								</div>
+								<p className={styles.dataModal}>{data}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</>
+		)
+	}
 
-  return null;
+	return null
 }
--- src/components/portifolio/index.tsx
+++ src/components/portifolio/index.tsx
@@ -1,86 +1,116 @@
-import CardPortifolio from "./cardPortifolio";
-import { useRef } from "react";
-import { useInView } from "framer-motion";
-import styles from "./portifolio.module.css";
-import sabio from "/sabio.png";
-import pokedex from "/pokedex.png";
-import htmlSVG from "/html.svg";
-// import tsSVG from '/ts.svg'
-import apiSVG from "/api.svg";
-import jsSVG from "/js.svg";
-// import figmaSVG from '/figma.svg'
-// import reactSVG from '/react.svg'
-import cssSVG from "/css.svg";
+import CardPortifolio from "./cardPortifolio"
+import { useRef } from "react"
+import { useInView } from "framer-motion"
+import styles from "./portifolio.module.css"
+import sabio from "/sabio.png"
+import pokedex from "/pokedex.png"
+import cida from "/cida.png"
+
+import htmlSVG from "/html.svg"
+import apiSVG from "/api.svg"
+import jsSVG from "/js.svg"
+import cssSVG from "/css.svg"
+import cSharpSVG from "/cSharp.svg"
+import dotNetSVG from "/dotNet.svg"
+import azureSVG from "/azure.svg"
 
 function Portifolio() {
-    const ref = useRef(null);
-    const ref2 = useRef(null);
-    const isInView = useInView(ref, { once: true });
+	const ref = useRef(null)
+	const ref2 = useRef(null)
+	const isInView = useInView(ref, { once: true })
 
-    const SabioDesc = () => (
-        <div className={styles.TextPortGap}>
-            <p>
-                Esse site foi desenvolvido por mim, baseado em um site chamado
-                "Pergunte ao Polvo", na época que criei o site, achava a ideia
-                muito interessante.
-            </p>
-            <ul>As Tecnologias Usadas Foram:</ul>
-            <li className={styles.Listinside}>HTML</li>
-            <li className={styles.Listinside}>CSS</li>
-            <li className={styles.Listinside}>JS</li>
-        </div>
-    );
+	const SabioDesc = () => (
+		<div className={styles.TextPortGap}>
+			<p>
+				Esse site foi desenvolvido por mim, baseado em um site chamado
+				"Pergunte ao Polvo", na época que criei o site, achava a ideia
+				muito interessante.
+			</p>
+			<ul>As Tecnologias Usadas Foram:</ul>
+			<li className={styles.Listinside}>HTML</li>
+			<li className={styles.Listinside}>CSS</li>
+			<li className={styles.Listinside}>JS</li>
+		</div>
+	)
 
-    const PokedexDesc = () => (
-        <div className={styles.TextPortGap}>
-            <p>
-                Esse site foi desenvolvido por mim, durante um curso da
-                plataforma digital da DIO.
-            </p>
-            <ul>As Tecnologias Usadas Foram:</ul>
-            <li className={styles.Listinside}>HTML</li>
-            <li className={styles.Listinside}>CSS</li>
-            <li className={styles.Listinside}>JS</li>
-            <li className={styles.Listinside}>API REST</li>
-        </div>
-    );
+	const PokedexDesc = () => (
+		<div className={styles.TextPortGap}>
+			<p>
+				Esse site foi desenvolvido por mim, durante um curso da
+				plataforma digital da DIO.
+			</p>
+			<ul>As Tecnologias Usadas Foram:</ul>
+			<li className={styles.Listinside}>HTML</li>
+			<li className={styles.Listinside}>CSS</li>
+			<li className={styles.Listinside}>JS</li>
+			<li className={styles.Listinside}>API REST</li>
+		</div>
+	)
 
-    return (
-        <section
-            ref={ref}
-            style={{
-                transform: isInView ? "none" : "translateX(-200px)",
-                opacity: isInView ? 1 : 0,
-                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
-            }}
-            id="projetos"
-            className={styles.sectionP}
-        >
-            <h2 className={styles.titlePortifolio}>Projetos</h2>
-            <div className={styles.projetos} ref={ref2}>
-                <CardPortifolio
-                    imagem={sabio}
-                    nome="Pergunte ao Sabio"
-                    desc={<SabioDesc />}
-                    data="09/2022"
-                    id="sabio"
-                    tecs={[htmlSVG, cssSVG, jsSVG]}
-                    linkSite="https://oleoz1n.github.io/pergunte-ao-sabio/"
-                    linkGithub="https://github.com/oleoz1n/pergunte-ao-sabio"
-                />
-                <CardPortifolio
-                    imagem={pokedex}
-                    nome="Pokedex DIO"
-                    desc={<PokedexDesc />}
-                    data="04/2023"
-                    id="pokedex"
-                    tecs={[htmlSVG, cssSVG, jsSVG, apiSVG]}
-                    linkSite="https://oleoz1n.github.io/Pokedex-Modify/"
-                    linkGithub="https://github.com/oleoz1n/Pokedex-Modify"
-                />
-            </div>
-        </section>
-    );
+	const CidaDesc = () => (
+		<div className={styles.TextPortGap}>
+			<p>
+				Essa API foi desenvolvida durante o Challenge da Plusoft na
+				FIAP. A ideia é gerar insights para o usuário com base nos
+				relatórios enviados por eles. Para isso, foram utilizados dois
+				modelos de LLM: Phi3 Mini e Gemini 1.5 Flash. Além disso, para
+				armazenamento de dados, foram usados o Azure Blob Storage e o
+				Azure SQL.
+			</p>
+			<ul>As Tecnologias Usadas Foram:</ul>
+			<li className={styles.Listinside}>C#</li>
+			<li className={styles.Listinside}>.NET</li>
+			<li className={styles.Listinside}>Azure</li>
+		</div>
+	)
+
+	return (
+		<section
+			ref={ref}
+			style={{
+				transform: isInView ? "none" : "translateX(-200px)",
+				opacity: isInView ? 1 : 0,
+				transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
+			}}
+			id="projetos"
+			className={styles.sectionP}
+		>
+			<h2 className={styles.titlePortifolio}>Projetos</h2>
+			<div className={styles.projetos} ref={ref2}>
+				<CardPortifolio
+					imagem={sabio}
+					nome="Pergunte ao Sabio"
+					desc={<SabioDesc />}
+					data="09/2022"
+					id="sabio"
+					tecs={[htmlSVG, cssSVG, jsSVG]}
+					linkSite="https://oleoz1n.github.io/pergunte-ao-sabio/"
+					linkGithub="https://github.com/oleoz1n/pergunte-ao-sabio"
+				/>
+				<CardPortifolio
+					imagem={pokedex}
+					nome="Pokedex DIO"
+					desc={<PokedexDesc />}
+					data="04/2023"
+					id="pokedex"
+					tecs={[htmlSVG, cssSVG, jsSVG, apiSVG]}
+					linkSite="https://oleoz1n.github.io/Pokedex-Modify/"
+					linkGithub="https://github.com/oleoz1n/Pokedex-Modify"
+				/>
+
+				<CardPortifolio
+					imagem={cida}
+					nome="Cida"
+					desc={<CidaDesc />}
+					data="10/2024"
+					id="cida"
+					tecs={[cSharpSVG, dotNetSVG, azureSVG]}
+					linkSite=""
+					linkGithub="https://github.com/Open-Group-Fiap/CIDA-DotNet8"
+				/>
+			</div>
+		</section>
+	)
 }
 
-export default Portifolio;
+export default Portifolio
