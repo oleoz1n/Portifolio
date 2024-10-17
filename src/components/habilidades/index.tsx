3a99599489834441f18cd92d6e3186c95cdb670a
oleoz1n <leopity0@gmail.com>
2024-10-17 17:50:43 -0300

    feat: add more habilidades

--- src/components/habilidades/index.tsx
+++ src/components/habilidades/index.tsx
@@ -1,55 +1,68 @@
-import Habilidade from "./habilidade";
-import styles from "./habilidades.module.css";
-import htmlSVG from "/html.svg";
-import tsSVG from "/ts.svg";
-import apiSVG from "/api.svg";
-import jsSVG from "/js.svg";
-import figmaSVG from "/figma.svg";
-import reactSVG from "/react.svg";
-import cssSVG from "/css.svg";
-import pythonSVG from "/python.svg";
-import javaSVG from "/java.svg";
-import sqlSVG from "/sql.svg";
-import { useRef } from "react";
-import { useInView } from "framer-motion";
+import Habilidade from "./habilidade"
+import styles from "./habilidades.module.css"
+import htmlSVG from "/html.svg"
+import tsSVG from "/ts.svg"
+import apiSVG from "/api.svg"
+import jsSVG from "/js.svg"
+import figmaSVG from "/figma.svg"
+import reactSVG from "/react.svg"
+import cssSVG from "/css.svg"
+import pythonSVG from "/python.svg"
+import javaSVG from "/java.svg"
+import sqlSVG from "/sql.svg"
+import dotNetSVG from "/dotNet.svg"
+import cSharp from "/cSharp.svg"
+import spring from "/spring.svg"
+import azure from "/azure.svg"
+import gcp from "/gcp.svg"
+import powerBi from "/powerBi.svg"
+
+import { useRef } from "react"
+import { useInView } from "framer-motion"
 
 export default function Habilidades() {
-    const ref = useRef(null);
-    const isInView = useInView(ref, { once: true });
+	const ref = useRef(null)
+	const isInView = useInView(ref, { once: true })
 
-    const listaDeHabilidades = [
-        { imagem: apiSVG, nome: "API REST" },
-        { imagem: htmlSVG, nome: "HTML5" },
-        { imagem: cssSVG, nome: "CSS3" },
-        { imagem: tsSVG, nome: "TypeScript" },
-        { imagem: jsSVG, nome: "JavaScript" },
-        { imagem: figmaSVG, nome: "Figma" },
-        { imagem: reactSVG, nome: "React" },
-        { imagem: pythonSVG, nome: "Python" },
-        { imagem: javaSVG, nome: "Java" },
-        { imagem: sqlSVG, nome: "SQL" },
-    ];
-    return (
-        <section
-            ref={ref}
-            style={{
-                transform: isInView ? "none" : "translateX(-200px)",
-                opacity: isInView ? 1 : 0,
-                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
-            }}
-            className={styles.sectionHabilidades}
-            id="habilidades"
-        >
-            <h2 className={styles.titleH}>Habilidades</h2>
-            <div className={styles.habilidades}>
-                {listaDeHabilidades.map((habilidade, index) => (
-                    <Habilidade
-                        key={index}
-                        imagem={habilidade.imagem}
-                        nome={habilidade.nome}
-                    />
-                ))}
-            </div>
-        </section>
-    );
+	const listaDeHabilidades = [
+		{ imagem: apiSVG, nome: "API REST" },
+		{ imagem: htmlSVG, nome: "HTML5" },
+		{ imagem: cssSVG, nome: "CSS3" },
+		{ imagem: tsSVG, nome: "TypeScript" },
+		{ imagem: jsSVG, nome: "JavaScript" },
+		{ imagem: figmaSVG, nome: "Figma" },
+		{ imagem: reactSVG, nome: "React" },
+		{ imagem: pythonSVG, nome: "Python" },
+		{ imagem: javaSVG, nome: "Java" },
+		{ imagem: sqlSVG, nome: "SQL" },
+		{ imagem: dotNetSVG, nome: ".NET 8" },
+		{ imagem: cSharp, nome: "C#" },
+		{ imagem: spring, nome: "Spring Boot" },
+		{ imagem: azure, nome: "Azure" },
+		{ imagem: gcp, nome: "Google Cloud" },
+		{ imagem: powerBi, nome: "Power BI" },
+	]
+	return (
+		<section
+			ref={ref}
+			style={{
+				transform: isInView ? "none" : "translateX(-200px)",
+				opacity: isInView ? 1 : 0,
+				transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
+			}}
+			className={styles.sectionHabilidades}
+			id="habilidades"
+		>
+			<h2 className={styles.titleH}>Habilidades</h2>
+			<div className={styles.habilidades}>
+				{listaDeHabilidades.map((habilidade, index) => (
+					<Habilidade
+						key={index}
+						imagem={habilidade.imagem}
+						nome={habilidade.nome}
+					/>
+				))}
+			</div>
+		</section>
+	)
 }
--- src/components/resume/redes/index.tsx
+++ src/components/resume/redes/index.tsx
@@ -1,64 +1,46 @@
-import linkedinIcon from "/linkedin.svg";
-import githunIcon from "/github.svg";
-import cvIcon from "/cv.svg";
-import styles from "./redes.module.css";
+import linkedinIcon from "/linkedin.svg"
+import githunIcon from "/github.svg"
+import styles from "./redes.module.css"
 
 function Github() {
-    return (
-        <div>
-            <a href="https://github.com/oleoz1n" target="_blank">
-                <div className={styles.infos}>
-                    <img
-                        src={githunIcon}
-                        className={styles.Icons}
-                        alt="GitHub-Icon"
-                    />
-                </div>
-            </a>
-        </div>
-    );
+	return (
+		<div>
+			<a href="https://github.com/oleoz1n" target="_blank">
+				<div className={styles.infos}>
+					<img
+						src={githunIcon}
+						className={styles.Icons}
+						alt="GitHub-Icon"
+					/>
+				</div>
+			</a>
+		</div>
+	)
 }
 
 function Linkedin() {
-    return (
-        <div>
-            <a href="https://www.linkedin.com/in/oleoz1n/" target="_blank">
-                <div className={styles.infos}>
-                    <img
-                        src={linkedinIcon}
-                        className={styles.Icons}
-                        alt="Linkedin-Icon"
-                    />
-                </div>
-            </a>
-        </div>
-    );
-}
-
-function Cv() {
-    return (
-        <div>
-            <a href="/Portifolio/CV.pdf" target="_blank">
-                <div className={styles.infos}>
-                    <img
-                        src={cvIcon}
-                        className={`${styles.cvIcon}  ${styles.Icons}`}
-                        alt="CV-Icon"
-                    />
-                </div>
-            </a>
-        </div>
-    );
+	return (
+		<div>
+			<a href="https://www.linkedin.com/in/oleoz1n/" target="_blank">
+				<div className={styles.infos}>
+					<img
+						src={linkedinIcon}
+						className={styles.Icons}
+						alt="Linkedin-Icon"
+					/>
+				</div>
+			</a>
+		</div>
+	)
 }
 
 function Redes() {
-    return (
-        <>
-            <Github />
-            <Linkedin />
-            <Cv />
-        </>
-    );
+	return (
+		<>
+			<Github />
+			<Linkedin />
+		</>
+	)
 }
 
-export default Redes;
+export default Redes
