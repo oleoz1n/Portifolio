2dc4435f8d5d3d0b182892c73ac8daff1faa84d1
oleoz1n <leopity0@gmail.com>
2024-10-23 08:01:11 -0300

    fix: enable lazy loading on habilidades

--- src/components/habilidades/habilidade/index.tsx
+++ src/components/habilidades/habilidade/index.tsx
@@ -1,16 +1,16 @@
-import styles from './habilidade.module.css'
+import styles from './habilidade.module.css';
 
 type HabilidadeProps = {
     imagem: string;
-    nome : string;
+    nome: string;
 }
 
-function Habilidade({imagem, nome}:HabilidadeProps){
+function Habilidade({ imagem, nome }: HabilidadeProps) {
     return (
 
         <div className={styles.habilidadeDiv}>
-        <p className={styles.habilidadeNome}>{nome}</p>
-        <img className={styles.habilidadeImg} src={imagem} alt={nome} />
+            <p className={styles.habilidadeNome}>{nome}</p>
+            <img className={styles.habilidadeImg} src={imagem} alt={nome} loading="lazy" />
         </div>
     )
 }
