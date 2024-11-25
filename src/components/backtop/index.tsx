52181fea298df6d8e559e085e97a78d2c60fd253
oleoz1n <leopity0@gmail.com>
2024-11-25 20:03:58 -0300

    fix: make the button backtop all clickable and add cursor pointer

--- src/components/backtop/index.tsx
+++ src/components/backtop/index.tsx
@@ -4,62 +4,67 @@ import styles from "./backtop.module.css";
 import upArrow from "/upArrow.svg";
 
 export default function ScrollPage() {
-  const [isVisible, setIsVisible] = useState(true);
+	const [isVisible, setIsVisible] = useState(true);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      if (scrollY > 100) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
-      }
-    };
+	useEffect(() => {
+		const handleScroll = () => {
+			const scrollY = window.scrollY;
+			if (scrollY > 100) {
+				setIsVisible(false);
+			} else {
+				setIsVisible(true);
+			}
+		};
 
-    window.addEventListener("scroll", handleScroll);
+		window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-  const handleAnimationStart = () => {
-    const backTopElement = document.getElementById("backTopAnimation");
-    if (backTopElement) {
-      const computedStyle = window.getComputedStyle(backTopElement);
-      const opacityValue = computedStyle.getPropertyValue("opacity");
+	const handleAnimationStart = () => {
+		const backTopElement = document.getElementById("backTopAnimation");
+		if (backTopElement) {
+			const computedStyle = window.getComputedStyle(backTopElement);
+			const opacityValue = computedStyle.getPropertyValue("opacity");
 
-      if (opacityValue === "0") {
-        backTopElement.style.display = "flex";
-      }
-    }
-  };
+			if (opacityValue === "0") {
+				backTopElement.style.display = "flex";
+			}
+		}
+	};
 
-  const handleAnimationComplete = () => {
-    const backTopElement = document.getElementById("backTopAnimation");
-    if (backTopElement) {
-      const computedStyle = window.getComputedStyle(backTopElement);
-      const opacityValue = computedStyle.getPropertyValue("opacity");
+	const handleAnimationComplete = () => {
+		const backTopElement = document.getElementById("backTopAnimation");
+		if (backTopElement) {
+			const computedStyle = window.getComputedStyle(backTopElement);
+			const opacityValue = computedStyle.getPropertyValue("opacity");
 
-      if (opacityValue === "0") {
-        backTopElement.style.display = "none";
-      }
-    }
-  };
+			if (opacityValue === "0") {
+				backTopElement.style.display = "none";
+			}
+		}
+	};
 
-  return (
-    <motion.div
-      className={styles.containerBacktop}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: isVisible ? 0 : 1 }}
-      transition={{ duration: 0.3 }}
-      onAnimationStart={handleAnimationStart}
-      onAnimationComplete={handleAnimationComplete}
-      id="backTopAnimation"
-    >
-      <a href="#header">
-        <img className={styles.backtopIcon} src={upArrow} alt="upArrow" />
-      </a>
-    </motion.div>
-  );
+	return (
+		<motion.div
+			className={styles.containerBacktop}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: isVisible ? 0 : 1 }}
+			transition={{ duration: 0.3 }}
+			onAnimationStart={handleAnimationStart}
+			onAnimationComplete={handleAnimationComplete}
+			id="backTopAnimation"
+			onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+		>
+			<a>
+				<img
+					className={styles.backtopIcon}
+					src={upArrow}
+					alt="upArrow"
+				/>
+			</a>
+		</motion.div>
+	);
 }
